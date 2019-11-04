f5c91020221155c36aa7f897a6ffa633ee803f18
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-04 19:21:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,13 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0
+        resp.write "Your cart is empty"
+      else
+        resp.write "@@cart"
+    elsif reg.path.match(/add/)
+
     else
       resp.write "Path Not Found"
     end
